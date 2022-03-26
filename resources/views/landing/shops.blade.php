1ff780711b6bb63e50482bf5bc3c815c4bf9f507
unknown <fatemehjazayeri707070@gmail.com>
2022-03-27 03:24:34 +0430

    Landing Pages BackEnd

--- resources/views/landing/shops.blade.php
+++ resources/views/landing/shops.blade.php
@@ -1,39 +1,5 @@
-<!DOCTYPE html>
-<html lang="fa">
-    <head>
-        <meta charset="utf-8">
-        <title> به سایت SF خوش آمدید </title>
-
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-        <link rel="stylesheet" href="{{asset('css/public.css')}}">
-    </head>
-    <body>
-
-        <div class="container py-4">
-
-            <ul class="nav nav-tabs">
-                <li class="nav-item">
-                    <a class="nav-link" href="{{url('/')}}"> صفحه اصلی </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{route('landing', 'products')}}"> محصولات </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link active" href="{{route('landing', 'shops')}}"> فروشگاه ها </a>
-                </li>
-            </ul>
-
-            <div class="card mt-3">
-                <div class="card-body">
-
-                    <h4> فروشگاه ها </h4>
-                    <hr>
-
-                </div>
-            </div>
-        </div>
-
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
-        <script src="{{asset('js/public.js')}}" charset="utf-8"></script>
-    </body>
-</html>
+@extends('layouts.landing')
+@section('content')
+<h4>فروشگاه ها</h4>
+<hr>
+@endsection
