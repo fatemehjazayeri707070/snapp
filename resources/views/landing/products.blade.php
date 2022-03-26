a0cefb3348dcc9c14e5b6cae2df44459dc63eb67
unknown <fatemehjazayeri707070@gmail.com>
2022-03-27 03:34:54 +0430

    Public Product Cards

--- resources/views/landing/products.blade.php
+++ resources/views/landing/products.blade.php
@@ -1,5 +1,24 @@
 @extends('layouts.landing')
 @section('content')
-<h4>محصولات</h4>
-<hr>
+
+    <h4> محصولات </h4>
+    <div class="row">
+        @foreach ($products as $product)
+            <div class="col-md-4 my-3 product-card">
+                <div class="d-flex justify-content-between">
+                    <h5> {{$product->title}} </h5>
+                    <p>
+                        @if ($product->discount)
+                            <span class="text-danger off mx-2"> {{number_format($product->price)}} </span>
+                        @endif
+                        <span> {{number_format($product->cost)}} </span>
+                    </p>
+                </div>
+                <hr>
+                <img src="{{asset($product->image)}}">
+                <p> {{$product->description}} </p>
+            </div>
+        @endforeach
+    </div>
+
 @endsection
